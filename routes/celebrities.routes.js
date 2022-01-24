180bca3d8aa7012884f926b233fefabf9a662da1
Sebas Corral <sebascorralbrito@gmail.com>
2022-01-24 14:16:52 +0100

    routes modified

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,47 +1,27 @@
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
-const Movie = require("../models/Movies.model");
-
-/* GET celebrities page */
-router.get("/celebrities", (req, res, next) => {
-    //  the url
-    Celebrity.find()
-    .then((celebritiesList) => {
-        res.render("celebrities/celebrities", {celebritiesList});
-    })
-    .catch((err) => {
-        console.log(err)
-    });
-   
-});
 
 router.get("/celebrities/create", (req, res, next) => {
-    res.render("celebrities/new-celebrity");
+    res.render("celebrities/new-celebrity")
 });
 
-router.post("/celebrities/create", (req, res, next) => {
+router.post("/celebrities/create", async (req, res, next) => {
     const { name, occupation, catchPhrase } = req.body;
-    //console.log("celebrityId", celebrityId);
-    Celebrity.create({ name, occupation, catchPhrase })
-        .then((createdCelebrity) => {
-            console.log("Celeb", createdCelebrity);
-            res.redirect("/celebrities"); //URL
-        })
-        .catch((err) => {
-            console.log(err)
-            res.redirect("/celebrities/create") 
-        });
+    try {
+        await Celebrity.create({ name, occupation, catchPhrase });
+        res.redirect("celebrities")
+    } catch (error) {
+        res.render("celebrities/new-celebrity");
+    }
+    
+      
 })
 
-router.get("/celebrities", (req, res) => {
-    const getThemAll = req.query.getThemAll;
-    Celebrity.find({ name: { $regex: getThemAll, $options: "i" } })
-        .then((celebritiesList) => {
-            //console.log("celebritiesList", celebritiesList);
-            res.render("celebrities/new-celebrity.hbs", { celebritiesList: celebritiesList })
-        })
-        .catch((err) => { console.log(err); })
-})
-
+router.get("/celebrities", (req, res, next) => {
+    Celebrity.find()
+    .then((celebrities) => {
+        res.render("celebrities/celebrities", {celebrities});
+    })     
+});
 
 module.exports = router;
